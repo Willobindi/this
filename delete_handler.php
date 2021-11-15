73cd14e7c70652b14cdad112df182cda410c70ca
Willobindi <godwillobindi5@gmail.com>
2021-11-15 06:55:47 -0500

    this

---
+++ delete_handler.php
@@ -0,0 +1,9 @@
+<?php
+if (isset($_GET["user_id"])){
+    $receivedId= $_GET["user_id"];
+    // to delete , connect to the database
+    
+    require_once "connection.php";
+    // prepare the delete query
+
+                                        }
